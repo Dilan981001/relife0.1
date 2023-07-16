86efedc1c9332b566148cc8d4f6121966eb70d96
charithsekara <charithmadhushasekara@gmail.com>
2023-07-16 13:38:50 +0530

    change h3
    deleted about us

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,9 +6,7 @@
           <div class="form-actions">
             <q-btn to="/home" label="HOME" class="q-mr-md" />
           </div>
-          <div class="form-actions">
-            <q-btn label="ABOUT US" class="q-mr-md" />
-          </div>
+        
           <div class="form-actions">
             <q-btn label="NOTIFICATIONS" class="q-mr-md"/>
           </div>
