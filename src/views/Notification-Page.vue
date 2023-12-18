25fd588783c9da19251368798728bf0434a53549
DilanFdo <priyankarad276@gmail.com>
2023-12-18 11:20:50 +0530

    notification page update

--- src/views/Notification-Page.vue
+++ src/views/Notification-Page.vue
@@ -80,26 +80,28 @@
         </div>
       </div>
       <div class="column-right">
-
+<div class="row">
+  <div class="col">
     <q-card class="my-card" flat bordered>
-      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+      <q-img src="../../public/h1.jpeg"  height="200px"/>
 
       <q-card-section>
         <q-btn
           fab
-          color="primary"
+          color="green"
           icon="place"
           class="absolute"
-          style="top: 0; right: 12px; transform: translateY(-50%);"
+          style="top: 0; right: 12px; transform: translateY(-50%)"
+          href="https://maps.app.goo.gl/vmotaq8gCRSmiQDR8"
         />
 
         <div class="row no-wrap items-center">
           <div class="col text-h6 ellipsis">
-            Cafe Basilico
+            
           </div>
           <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
             <q-icon name="place" />
-            250 ft
+            130km
           </div>
         </div>
 
@@ -108,22 +110,270 @@
 
       <q-card-section class="q-pt-none">
         <div class="text-subtitle1">
-          $・Italian, Cafe
+         
         </div>
         <div class="text-caption text-grey">
-          Small plates, salads & sandwiches in an intimate setting.
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
         </div>
       </q-card-section>
 
       <q-separator />
 
       <q-card-actions>
-        <q-btn flat round icon="event" />
-        <q-btn flat color="primary">
-          Reserve
+        <q-btn flat round icon=" " />
+        <q-btn flat color="green">
+          More
         </q-btn>
       </q-card-actions>
     </q-card>
+  </div>
+  <div class="col">
+    <q-card class="my-card" flat bordered>
+      <q-img src="../../public/h2.png" height="200px" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="green"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          href="https://maps.app.goo.gl/iUkKvVfgU7NkknpV7"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+           
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            124m
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+       
+        </div>
+        <div class="text-caption text-grey">
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon=" " />
+        <q-btn flat color="green">
+          More
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+  <div class="col">
+    <q-card class="my-card" flat bordered>
+      <q-img src="../../public/h3.jpeg" height="200px" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="green"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          href="https://maps.app.goo.gl/GozpFWK6wrosXYxCA"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+           
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            115km
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+        
+        </div>
+        <div class="text-caption text-grey">
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon=" " />
+        <q-btn flat color="green">
+          More
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</div>
+<br/>
+<br/>
+
+<div class="row">
+  <div class="col">
+    <q-card class="my-card" flat bordered>
+      <q-img src="../../public/h4.jpg" height="200px" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="green"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          href="https://maps.app.goo.gl/vmotaq8gCRSmiQDR8"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+        
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            13km
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+    
+        </div>
+        <div class="text-caption text-grey">
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon=" " />
+        <q-btn flat color="green">
+          More
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+  <div class="col">
+    <q-card class="my-card" flat bordered>
+      <q-img src="../../public/h2.png" height="200px" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="green"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          href="https://maps.app.goo.gl/GozpFWK6wrosXYxCA"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+        
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            95km
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+         
+        </div>
+        <div class="text-caption text-grey">
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon=" " />
+        <q-btn flat color="green">
+          More
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+  <div class="col">
+    <q-card class="my-card" flat bordered>
+      <q-img src="../../public/h1.jpeg" height="200px" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="green"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+          href="https://maps.app.goo.gl/ixVcHNX9TSnypgFFA"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+       
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            235km
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+         
+        </div>
+        <div class="text-caption text-grey">
+          Vitae sapien pellentesque habitant morbi nunc. Viverra aliquet
+          porttitor rhoncus libero justo laoreet sit amet vitae.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="  " />
+        <q-btn flat color="green">
+          More
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+</div>
+
+
+   
+
+    
 
       </div>
     </div>
@@ -425,4 +675,6 @@ h2 {
   font-weight: lighter;
   text-align: justify;
 }
+
+
 </style>
